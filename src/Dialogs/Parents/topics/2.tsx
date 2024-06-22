3583ac4ef8b4cc28542025135ee165ccd1b5203f
afifa00tahniat <afifa00tahniat@gmail.com>
2024-06-22 16:46:42 +0500

    content change

--- src/Dialogs/Parents/topics/2.tsx
+++ src/Dialogs/Parents/topics/2.tsx
@@ -1,20 +1,10 @@
-import { Typography } from "@mui/material";
 import DialogLayout from "../../layout";
 import { parentingCategories } from "../../../Constants/categories";
-import useResponsiveFontSize from "../../../hooks/useResponsiveFontSize";
 
 const Topic2 = ({ goback }: { goback: () => void }) => {
-  const [paraSize] = useResponsiveFontSize(30, 65);
   return (
     <DialogLayout heading={parentingCategories[1].name} handleClose={goback}>
-      <Typography
-        style={{
-          fontSize: paraSize,
-          textAlign: "center",
-          fontFamily: "Noto Nastaliq Urdu",
-          color: "#ededed",
-        }}
-      ></Typography>
+      <div />
     </DialogLayout>
   );
 };
